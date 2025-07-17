5789a3d3babc4a2c5f181f9f1c3a08e1e23eefcc
harshdeep3 <harshdeep366@gmail.com>
2025-07-17 15:05:38 +0100

    adding a evaluation make prediction function and visual grid of correct and incorrect preds

--- fashionMNSIT_classification.py
+++ fashionMNSIT_classification.py
@@ -1,5 +1,6 @@
 import pathlib
 from tqdm import tqdm
+import random
 
 import matplotlib.pyplot as plt
 import torch
@@ -60,8 +61,51 @@ def show_gird_of_images(data, class_name, grey_scale=False):
     plt.show()
 
 
+def show_eval_data_gird_of_images(test_samples, class_name, pred_classes, test_labels):
+    """
+    show a grid of images
+
+    Args:
+        data (list): list of data points
+    """
+    # Plot predictions
+    plt.figure(figsize=(9, 9))
+    nrows = 3
+    ncols = 3
+    for i, sample in enumerate(test_samples):
+        # Create a subplot
+        plt.subplot(nrows, ncols, i + 1)
+
+        # Plot the target image
+        plt.imshow(sample.squeeze(), cmap="gray")
+
+        # Find the prediction label (in text form, e.g. "Sandal")
+        pred_label = class_name[pred_classes[i]]
+
+        # Get the truth label (in text form, e.g. "T-shirt")
+        truth_label = class_name[test_labels[i]]
+
+        # Create the title text of the plot
+        title_text = f"Pred: {pred_label} | Truth: {truth_label}"
+
+        # Check for equality and change title colour accordingly
+        if pred_label == truth_label:
+            plt.title(title_text, fontsize=10, c="g")  # green text if correct
+        else:
+            plt.title(title_text, fontsize=10, c="r")  # red text if wrong
+        plt.axis(False)
+
+    plt.show()
+
+
 def train_model(
-    model, train_dataloader, device, optimizer, loss_fn, accuracy_fn, epochs=5
+    model: torch.nn.Module,
+    train_dataloader: torch.utils.data.DataLoader,
+    device: str,
+    optimizer,
+    loss_fn: torch.nn.Module,
+    accuracy_fn,
+    epochs=5,
 ):
     """
     Train a model on the FashionMNIST dataset
@@ -120,7 +164,12 @@ def train_model(
     return model
 
 
-def test(model, test_dataloader, loss_fn, accuracy_fn):
+def test(
+    model: torch.nn.Module,
+    test_dataloader: torch.utils.data.DataLoader,
+    loss_fn: torch.nn.Module,
+    accuracy_fn,
+):
     """
     Test the model on the FashionMNIST dataset
     """
@@ -149,6 +198,31 @@ def test(model, test_dataloader, loss_fn, accuracy_fn):
         "model_loss": loss.item(),
         "model_acc": acc,
     }
+
+
+def make_predictions(model: torch.nn.Module, data: list, device: torch.device):
+    pred_probs = []
+    model.eval()
+    with torch.inference_mode():
+        for sample in data:
+            # Prepare sample
+            sample = torch.unsqueeze(sample, dim=0).to(
+                device
+            )  # Add an extra dimension and send sample to device
+
+            # Forward pass (model outputs raw logit)
+            pred_logit = model(sample)
+
+            # Get prediction probability (logit -> prediction probability)
+            pred_prob = torch.softmax(
+                pred_logit.squeeze(), dim=0
+            )  # note: perform softmax on the "logits" dimension, not "batch" dimension (in this case we have a batch size of 1, so can perform on dim=0)
+
+            # Get pred_prob off GPU for further calculations
+            pred_probs.append(pred_prob.cpu())
+
+    # Stack the pred_probs to turn list into a tensor
+    return torch.stack(pred_probs)
 
 
 if __name__ == "__main__":
@@ -188,8 +262,8 @@ if __name__ == "__main__":
     # show_gird_of_images(train_data, class_names, False)
 
     # hyperparameters
-    batch_size = 64
-    epochs = 5
+    batch_size = 128
+    epochs = 10
     lr = 0.001
 
     # Turn datasets into iterables (batches)
@@ -212,45 +286,47 @@ if __name__ == "__main__":
         input_shape=1 * 28 * 28, hidden_units=128, output_shape=len(class_name)
     ).to(device)
     loss_fn = nn.CrossEntropyLoss()
-    # optimizer = torch.optim.Adam(model_v0.parameters(), lr=lr)
+    optimizer = torch.optim.Adam(model_v0.parameters(), lr=lr)
 
-    # model_v0 = train_model(
-    #     model=model_v0,
-    #     train_dataloader=train_dataloader,
-    #     device=device,
-    #     optimizer=optimizer,
-    #     loss_fn=loss_fn,
-    #     accuracy_fn=accuracy_score,
-    #     epochs=epochs,
-    # )
+    model_v0 = train_model(
+        model=model_v0,
+        train_dataloader=train_dataloader,
+        device=device,
+        optimizer=optimizer,
+        loss_fn=loss_fn,
+        accuracy_fn=accuracy_score,
+        epochs=epochs,
+    )
 
-    # print(
-    #     "Testing -> ", test(model_v0, test_dataloader, loss_fn, accuracy_fn=accuracy_score)
-    # )
+    print(
+        "Testing -> ",
+        test(model_v0, test_dataloader, loss_fn, accuracy_fn=accuracy_score),
+    )
 
-    # model_v1 = FashionMNISTModelV1(
-    #     input_shape=1 * 28 * 28, hidden_units=128, output_shape=len(class_name)
-    # ).to(device)
-    
-    # optimizer = torch.optim.Adam(model_v1.parameters(), lr=lr)
-    # model_v1 = train_model(
-    #     model=model_v1,
-    #     train_dataloader=train_dataloader,
-    #     device=device,
-    #     optimizer=optimizer,
-    #     loss_fn=loss_fn,
-    #     accuracy_fn=accuracy_score,
-    #     epochs=epochs,
-    # )
+    model_v1 = FashionMNISTModelV1(
+        input_shape=1 * 28 * 28, hidden_units=128, output_shape=len(class_name)
+    ).to(device)
 
-    # print(
-    #     "Testing -> ", test(model_v1, test_dataloader, loss_fn, accuracy_fn=accuracy_score)
-    # )
-    
+    optimizer = torch.optim.Adam(model_v1.parameters(), lr=lr)
+    model_v1 = train_model(
+        model=model_v1,
+        train_dataloader=train_dataloader,
+        device=device,
+        optimizer=optimizer,
+        loss_fn=loss_fn,
+        accuracy_fn=accuracy_score,
+        epochs=epochs,
+    )
+
+    print(
+        "Testing -> ",
+        test(model_v1, test_dataloader, loss_fn, accuracy_fn=accuracy_score),
+    )
+
     model_v2 = FashionMNISTModelV2(
         input_shape=1, hidden_units=10, output_shape=len(class_name)
     ).to(device)
-    
+
     optimizer = torch.optim.Adam(model_v2.parameters(), lr=lr)
 
     model_v2 = train_model(
@@ -264,5 +340,20 @@ if __name__ == "__main__":
     )
 
     print(
-        "Testing -> ", test(model_v2, test_dataloader, loss_fn, accuracy_fn=accuracy_score)
+        "Testing -> ",
+        test(model_v2, test_dataloader, loss_fn, accuracy_fn=accuracy_score),
     )
+
+    test_samples = []
+    test_labels = []
+    for sample, label in random.sample(list(test_data), k=9):
+        test_samples.append(sample)
+        test_labels.append(label)
+
+    # Make predictions on test samples with model 2
+    pred_probs = make_predictions(model=model_v2, data=test_samples, device=device)
+
+    # Turn the prediction probabilities into prediction labels by taking the argmax()
+    pred_classes = pred_probs.argmax(dim=1)
+
+    show_eval_data_gird_of_images(test_samples, class_name, pred_classes, test_labels)
